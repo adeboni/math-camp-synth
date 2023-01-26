456f64ebb55071fc2d2022fe054d4fcfdb4e57ba
Alex DeBoni <alexander.deboni@gmail.com>
2023-01-26 14:09:38 -0800

    Revamped synth code

--- 2023-Jukebox/software/main_board/main_board.ino
+++ 2023-Jukebox/software/main_board/main_board.ino
@@ -1,169 +1,130 @@
-#define NUM_CHANNELS 4
-
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+#include <RotaryEncoder.h>
+#include <Bounce.h>
 #include "StringUtils.h"
-#include "AudioChain.h"
-#include "OledMenu.h"
-#include "MenuDefs.h"
-#include "Globals.h"
-#include <EEPROM.h>
 
-void readInputs() { 
-  for (int i = 0; i < 8; i++)
-    cv[i] = map(analogRead(cvPins[i]), 0, 1023, 0, 100);
+#define UP_BTN     29
+#define DOWN_BTN   30
+#define SELECT_BTN 28
 
-  volKnob = map(analogRead(27), 0, 1023, 0, 100);
-  sgtl5000_1.dacVolume((float)volKnob / 100);
-  sgtl5000_2.dacVolume((float)volKnob / 100);
+#define SCREEN_WIDTH  128
+#define SCREEN_HEIGHT 64
+
+char wavFileName[30];
+bool wavPlaying = false;
+
+int numAudioFiles = 0;
+char fileList[30][64];
+int hoverRow = 0;
+int selectedRow = 0;
+
+AudioPlaySdRaw       playSdWav;
+AudioInputI2SQuad    i2s_in;
+AudioOutputI2SQuad   i2s_out;
+AudioMixer4          i2s_mixer;
+AudioMixer4          out_mixer;
+AudioEffectReverb    reverb;
+AudioConnection      patchCord01(i2s_in, 0, i2s_mixer, 0);
+AudioConnection      patchCord02(i2s_in, 1, i2s_mixer, 1);
+AudioConnection      patchCord03(i2s_in, 2, i2s_mixer, 2);
+AudioConnection      patchCord04(i2s_in, 3, i2s_mixer, 3);
+AudioConnection      patchCord05(i2s_mixer, 0, out_mixer, 0);
+AudioConnection      patchCord06(playSdWav, 0, out_mixer, 1);
+AudioConnection      patchCord07(out_mixer, 0, i2s_out, 0);
+AudioConnection      patchCord08(out_mixer, 0, i2s_out, 1);
+AudioConnection      patchCord09(out_mixer, 0, i2s_out, 2);
+AudioConnection      patchCord10(out_mixer, 0, i2s_out, 3);
+AudioControlSGTL5000 sgtl5000_1;
+AudioControlSGTL5000 sgtl5000_2;
+
+Adafruit_SH1106G display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+RotaryEncoder encoder(UP_BTN, DOWN_BTN, RotaryEncoder::LatchMode::TWO03);
+Bounce button(SELECT_BTN, 10);
+
+void setAudioInput(int input) {
+  if (input == 0) {
+    stopWavFiles();
+    out_mixer.gain(0, 1);
+    out_mixer.gain(1, 0);
+    i2s_mixer.gain(0, 1);
+    i2s_mixer.gain(1, 1);
+    i2s_mixer.gain(2, 0);
+    i2s_mixer.gain(3, 0);
+  } else if (input == 1) {
+    stopWavFiles();
+    out_mixer.gain(0, 1);
+    out_mixer.gain(1, 0);
+    i2s_mixer.gain(0, 0);
+    i2s_mixer.gain(1, 0);
+    i2s_mixer.gain(2, 1);
+    i2s_mixer.gain(3, 1);
+  } else {
+    out_mixer.gain(0, 0);
+    out_mixer.gain(1, 1);
+    i2s_mixer.gain(0, 0);
+    i2s_mixer.gain(1, 0);
+    i2s_mixer.gain(2, 0);
+    i2s_mixer.gain(3, 0);
+
+    sprintf(wavFileName, "/Audio/%s.raw", fileList[input]);
+    playSdWav.play(wavFileName);
+  }
 }
 
-void updateAudioSettings(int channel, const char *option, const char *newVal) {
-  AudioNoInterrupts();
-  if (strcmp(option, "Source") == 0) {
-     audioChain[channel].wavPlaying = false;
-     audioChain[channel].playSdWav.stop();
-     delete audioChain[channel].vcoToVcf;
-    
-     if (strcmp(newVal, "EXT IN") == 0) {
-       audioChain[channel].vcoToVcf = new AudioConnection(audioInput, audioIOMapping[channel], audioChain[channel].filter, 0);
-       audioChain[channel].wavPlaying = false;
-     } else {
-       sprintf(audioChain[channel].wavFileName, "/Audio/%s.raw", newVal);
-       audioChain[channel].playSdWav.play(audioChain[channel].wavFileName);
-       audioChain[channel].vcoToVcf = new AudioConnection(audioChain[channel].playSdWav, 0, audioChain[channel].filter, 0);
-       delay(10);
-       audioChain[channel].wavPlaying = true;
-     }
-     
-  } else if (strcmp(option, "Filter Frq") == 0) {
-     audioChain[channel].vcfFrqMod = audioChain[channel].getKnobValue(newVal);
-     if (audioChain[channel].vcfFrqMod == -1)
-        audioChain[channel].setVcfFrq(percentStrToInt(newVal));
-        
-  } else if (strcmp(option, "Filter Res") == 0) {
-     audioChain[channel].vcfResMod = audioChain[channel].getKnobValue(newVal);
-     if (audioChain[channel].vcfResMod == -1)
-        audioChain[channel].setVcfRes(percentStrToInt(newVal));
-        
-  } else if (strcmp(option, "Effect") == 0) {
-     delete audioChain[channel].vcfToEffect;
-     delete audioChain[channel].effectToVca;
-    
-    if (strcmp(newVal, "None") == 0) {
-       audioChain[channel].selectedEffect = -1;
-       audioChain[channel].vcfToEffect = new AudioConnection(audioChain[channel].filter, 0, audioChain[channel].amp, 0);
-    } else if (strcmp(newVal, "Reverb") == 0) {
-       audioChain[channel].selectedEffect = 0;
-       audioChain[channel].vcfToEffect = new AudioConnection(audioChain[channel].filter, 0, audioChain[channel].reverb, 0);
-       audioChain[channel].effectToVca = new AudioConnection(audioChain[channel].reverb, 0, audioChain[channel].amp, 0);
-    } else if (strcmp(newVal, "Granular") == 0) {
-       audioChain[channel].selectedEffect = 1;
-       audioChain[channel].vcfToEffect = new AudioConnection(audioChain[channel].filter, 0, audioChain[channel].granular, 0);
-       audioChain[channel].effectToVca = new AudioConnection(audioChain[channel].granular, 0, audioChain[channel].amp, 0);
-    } 
-    
-  } else if (strcmp(option, "Effect Mod") == 0) {
-    audioChain[channel].effectMod = audioChain[channel].getKnobValue(newVal);
-     if (audioChain[channel].effectMod == -1)
-        audioChain[channel].setEffectMod(percentStrToInt(newVal));
-        
-  } else if (strcmp(option, "Gain Mod") == 0) {
-    audioChain[channel].vcaMod = audioChain[channel].getKnobValue(newVal);
-     if (audioChain[channel].vcaMod == -1)
-        audioChain[channel].setVcaMod(percentStrToInt(newVal));
-  }
-
-  AudioInterrupts();
-}
-
-void updateAudioSettings() {
-  for (int i = 0; i < NUM_CHANNELS; i++) {
-    updateAudioSettings(i, "Source", preset["Audio"][i]["Source"].as<char*>());
-    updateAudioSettings(i, "Source Mod", preset["Audio"][i]["Source Mod"].as<char*>());
-    updateAudioSettings(i, "Filter Frq", preset["Audio"][i]["Filter Frq"].as<char*>());
-    updateAudioSettings(i, "Filter Res", preset["Audio"][i]["Filter Res"].as<char*>());
-    updateAudioSettings(i, "Effect", preset["Audio"][i]["Effect"].as<char*>());
-    updateAudioSettings(i, "Effect Mod", preset["Audio"][i]["Effect Mod"].as<char*>());
-    updateAudioSettings(i, "Gain Mod", preset["Audio"][i]["Gain Mod"].as<char*>());
-
-    delete audioChain[i].vcaToOutput;
-    audioChain[i].vcaToOutput = new AudioConnection(audioChain[i].amp, 0, audioOutput, audioIOMapping[i]);
-  }
+void displayMessage(const char* msg) {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print(msg);
+  display.display();
 }
 
 void stopWavFiles() {
-  for (int i = 0; i < NUM_CHANNELS; i++)
-    audioChain[i].playSdWav.stop();
+  wavPlaying = false;
+  playSdWav.stop();
 }
 
 void resumeWavFiles() {
-  for (int i = 0; i < NUM_CHANNELS; i++) {
-    if (audioChain[i].wavPlaying && !audioChain[i].playSdWav.isPlaying())
-      audioChain[i].playSdWav.play(audioChain[i].wavFileName);
-  }
-}
-
-void savePreset() {
-  stopWavFiles();
-  int presetNum = 0;
-  File presetsRoot = SD.open("/Presets/");
-  while (true) {
-     File entry = presetsRoot.openNextFile();
-     if (!entry) break;
-     if (strEndsWith(entry.name(), ".txt")) {
-        char buf[16];
-        trimExtension(entry.name(), buf);
-        presetNum = max(atoi(buf), presetNum);
-     }
-     entry.close();
-  }
-  presetsRoot.close();
-
-  char filename[20];
-  sprintf(filename, "/Presets/%d.txt", ++presetNum);
-  SD.remove(filename);
-
-  File file = SD.open(filename, FILE_WRITE);
-  if (!file) {
-    Serial.println(F("Failed to create file"));
-    return;
-  }
-
-  serializeJson(preset, file);
-  file.close();
-
-  currPreset = presetNum;
-  EEPROM.write(0, currPreset);
-}
-
-void loadPreset(int presetNum) {
-  stopWavFiles();
-  Serial.print("Loading preset ");
-  Serial.println(presetNum);
-  
-  char filename[20];
-  sprintf(filename, "/Presets/%d.txt", presetNum);
-
-  File file = SD.open(filename);
-  if (!file) {
-    if (presetNum > 1) {
-      loadPreset(presetNum - 1);
-      return;
-    }
-    Serial.println(F("Failed to open file"));
-    return;
-  }
-
-  deserializeJson(preset, file);
-  file.close();
-
-  currPreset = presetNum;
-  EEPROM.write(0, currPreset);
-  updateAudioSettings();
+  if (wavPlaying && !playSdWav.isPlaying())
+    playSdWav.play(wavFileName);
 }
 
 void setup() {
-  Serial.begin(9600);
   AudioMemory(50);
+  
+  pinMode(SELECT_BTN, INPUT_PULLUP);
+  display.begin(0x3C, true);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SH110X_WHITE);
+  display.display();
+  
+  if (!SD.begin(BUILTIN_SDCARD)) {
+    displayMessage("SD init error");
+    while (true) ;
+  }
+
+  strcpy(fileList[numAudioFiles++], "EXT 1");
+  strcpy(fileList[numAudioFiles++], "EXT 2");
+  File audioRoot = SD.open("/Audio/");
+  while (true) {
+     File entry = audioRoot.openNextFile();
+     if (!entry) break;
+     if (strEndsWith(entry.name(), ".raw")) {
+        char buf[16];
+        trimExtension(entry.name(), buf);
+        strcpy(fileList[numAudioFiles++], buf);
+     }
+     entry.close();
+  }
+  audioRoot.close();
+
+  displayMessage(fileList[0]);
 
   AudioNoInterrupts();
   sgtl5000_1.setAddress(HIGH);
@@ -172,7 +133,7 @@ void setup() {
   sgtl5000_1.adcHighPassFilterDisable();
   sgtl5000_1.lineInLevel(5);
   sgtl5000_1.dacVolumeRamp();
-  sgtl5000_1.dacVolume(0);
+  sgtl5000_1.dacVolume(1);
 
   sgtl5000_2.setAddress(LOW);
   sgtl5000_2.enable();
@@ -180,68 +141,51 @@ void setup() {
   sgtl5000_2.adcHighPassFilterDisable();
   sgtl5000_2.lineInLevel(5);
   sgtl5000_2.dacVolumeRamp();
-  sgtl5000_2.dacVolume(0);
+  sgtl5000_2.dacVolume(1);
   AudioInterrupts();
+}
 
-  menu.init();
-  menu.setCurrentMenu("Main Menu", &listMain, false);
-  loadPreset(EEPROM.read(0));
+void updateMenu() {
+  int i = 0;
+  static int pos = 0;
+  encoder.tick();
+  int newPos = encoder.getPosition();
+  if (pos != newPos) {
+    i = (int)(encoder.getDirection());
+    pos = newPos;
+  }
+
+  if (i != 0) 
+    hoverRow = constrain(hoverRow + i, 0, numAudioFiles - 1);
+  
+  if (button.update() && button.fallingEdge()) {
+    selectedRow = hoverRow;
+    setAudioInput(selectedRow);
+  }
+    
+  display.clearDisplay();
+  display.setCursor(0, 0);
+
+  int startPos = 0;
+  if (hoverRow > 4) 
+    startPos = hoverRow - 4;
+  
+  for (int i = startPos; i < min(startPos + 6, numAudioFiles); i++) {
+    display.setCursor(0, (i - startPos) * 10);
+    display.print(i == hoverRow ? '>' : ' ');
+    if (selectedRow == i)
+      display.setTextColor(SH110X_BLACK, SH110X_WHITE);
+    display.print(fileList[i]);
+    display.setTextColor(SH110X_WHITE);
+  }
+  display.display();
 }
 
 void loop() {
-  menu.doMenu();
-  readInputs();
+  int vol = map(analogRead(27), 0, 1023, 0, 100);
+  sgtl5000_1.dacVolume((float)vol / 100);
+  sgtl5000_2.dacVolume((float)vol / 100);
+
   resumeWavFiles();
+  updateMenu();
 }
-
-void getSetAudioOption() {
-  int i = menu.currentSubMenu;
-  int j = menu.metaItemIndex;
-
-  if (menu.updatingSetting) {
-     menu.getItemListText();
-     preset["Audio"][i][audioMenu[j].text] = menu.textBuffer;
-     currPreset = -1;
-     updateAudioSettings(i, audioMenu[j].text, menu.textBuffer);
-  } else {
-     strcpy(menu.textBuffer, preset["Audio"][i][audioMenu[j].text].as<char*>());
-  }
-}
-
-void getSetPreset() {
-  if (menu.updatingSetting) {
-     menu.getItemListText();
-     if (strcmp(menu.textBuffer, "None") == 0)
-        return;
-     currPreset = atoi(menu.textBuffer);
-     loadPreset(currPreset);
-  } else {
-     if (currPreset == -1)
-       strcpy(menu.textBuffer, "None");
-     else
-       sprintf(menu.textBuffer, "%d", currPreset);
-  }
-}
-
-
-void loadMainMenu() {
-  menu.currentSubMenu = -1;
-  menu.setCurrentMenu("Main Menu", &listMain, false);
-}
-
-void loadSubMenu() {
-  menu.currentSubMenu = menu.currentItemIndex;
-  menu.setCurrentMenu(mainMenu[menu.currentItemIndex].text, 
-    &mainMenuList[menu.currentItemIndex], 
-    menu.currentItemIndex == 5);
-}
-
-void readVolPot() { sprintf(menu.textBuffer, "%d", volKnob); }
-void readCV1() { sprintf(menu.textBuffer, "%d", cv[0]); }
-void readCV2() { sprintf(menu.textBuffer, "%d", cv[1]); }
-void readCV3() { sprintf(menu.textBuffer, "%d", cv[2]); }
-void readCV4() { sprintf(menu.textBuffer, "%d", cv[3]); }
-void readCV5() { sprintf(menu.textBuffer, "%d", cv[4]); }
-void readCV6() { sprintf(menu.textBuffer, "%d", cv[5]); }
-void readCV7() { sprintf(menu.textBuffer, "%d", cv[6]); }
-void readCV8() { sprintf(menu.textBuffer, "%d", cv[7]); }
---
+++ 2023-Jukebox/software/main_board_old/StringUtils.h
@@ -0,0 +1,8 @@
+#ifndef STRINGUTILS_H_
+#define STRINGUTILS_H_
+
+int strEndsWith(const char*, const char*);
+void trimExtension(const char*, char*);
+int percentStrToInt(const char*);
+
+#endif /* STRINGUTILS_H_ */
