a05d91022c7c3022524a34aee40c08c420aade67
Alex DeBoni <alexander.deboni@gmail.com>
2023-07-29 22:18:57 -0700

    Moved defines to another file

---
+++ 2023-Jukebox/software/display_adapter/Defines.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#define ShiftRegisterPWM_LATCH_PORT PORTB
+#define ShiftRegisterPWM_LATCH_MASK 0B00000001
+#define ShiftRegisterPWM_DATA_PORT PORTB
+#define ShiftRegisterPWM_DATA_MASK 0B00000100
+#define ShiftRegisterPWM_CLOCK_PORT PORTB
+#define ShiftRegisterPWM_CLOCK_MASK 0B00000010
+
+#define LED_DAT_PIN 10
+#define LED_CLK_PIN 9
+#define LED_LTC_PIN 8
+
+#define NES_DAT_PIN 13
+#define NES_CLK_PIN 7
+#define NES_LTC_PIN 6
+
+#define BTN_PIN  A7
+
+#define PWR_BTN 87.5
+#define SKP_BTN 85.7
+#define LTL_BTN 80.0
+#define LTR_BTN 50.0
+#define NBL_BTN 83.4
+#define NBR_BTN 66.7
+#define ENT_BTN 75.0
+
+#define PWR_BTN_LED 7
+#define SKP_BTN_LED 6
+#define LTL_BTN_LED 4
+#define LTR_BTN_LED 1
+#define NBL_BTN_LED 5
+#define NBR_BTN_LED 2
+#define ENT_BTN_LED 3
+
+#define MOTOR_0    13
+#define MOTOR_1    14
+#define MOTOR_2    15
+#define LAMP       12
+
+#define DOT_0      39
+#define DOT_1      38
+#define DOT_2      37
+#define DOT_3      36
+#define DOT_4      35
+#define DOT_5      34
+
+#define MOUTH_B_0  21
+#define MOUTH_R_0  22
+#define MOUTH_W_0  23
+
+#define MOUTH_B_1  18
+#define MOUTH_R_1  19
+#define MOUTH_W_1  20
+
+#define MOUTH_B_2  31
+#define MOUTH_R_2  16
+#define MOUTH_W_2  17
+
+#define MOUTH_B_3  28
+#define MOUTH_R_3  29
+#define MOUTH_W_3  30
+
+#define MOUTH_B_4  25
+#define MOUTH_R_4  26
+#define MOUTH_W_4  27
