39d1fb0a002d713592ddb79de49f8867dda4225d
Alex DeBoni <alexander.deboni@gmail.com>
2025-01-15 21:26:49 -0800

    Update esp_wand_receiver.ino

--- 2025-Jukebox/software/esp_wand_receiver/esp_wand_receiver.ino
+++ 2025-Jukebox/software/esp_wand_receiver/esp_wand_receiver.ino
@@ -1,8 +1,10 @@
 #include "wifi_credentials.h"
-#include <SPI.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include <map>
+#include <driver/gpio.h>
+#include <driver/spi_common.h>
+#include <driver/spi_slave.h>
 
 #define ESP_SCK_PIN   18
 #define ESP_MOSI_PIN  14
@@ -26,6 +28,7 @@ std::map<uint32_t, wand_data_t> packetMap;
 /////////////////////////////////////////////////////////////////////
 
 TaskHandle_t Core0Task;
+SemaphoreHandle_t _readySemaphore;
 
 void setup() {
   WiFi.softAPConfig(TARGET_IP, TARGET_GATEWAY, TARGET_SUBNET);
@@ -45,10 +48,30 @@ void core0(void * pvParameters) {
   
   pinMode(ESP_BUSY_PIN, OUTPUT);
   digitalWrite(ESP_BUSY_PIN, LOW);
-  SPI.begin(ESP_SCK_PIN, ESP_MISO_PIN, ESP_MOSI_PIN, ESP_CS_PIN);
-  pinMode(ESP_CS_PIN, INPUT);
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setBitOrder(MSBFIRST);
+
+  spi_bus_config_t busCfg;
+  spi_slave_interface_config_t slvCfg;
+
+  _readySemaphore = xSemaphoreCreateCounting(1, 0);
+
+  memset(&busCfg, 0x00, sizeof(busCfg));
+  busCfg.mosi_io_num = ESP_MOSI_PIN;
+  busCfg.miso_io_num = ESP_MISO_PIN;
+  busCfg.sclk_io_num = ESP_SCK_PIN;
+
+  memset(&slvCfg, 0x00, sizeof(slvCfg));
+  slvCfg.mode = 0;
+  slvCfg.spics_io_num = ESP_CS_PIN;
+  slvCfg.queue_size = 1;
+  slvCfg.flags = 0;
+  slvCfg.post_setup_cb = onSetupComplete;
+  slvCfg.post_trans_cb = NULL;
+
+  gpio_set_pull_mode((gpio_num_t)ESP_MOSI_PIN, GPIO_FLOATING);
+  gpio_set_pull_mode((gpio_num_t)ESP_SCK_PIN,  GPIO_PULLDOWN_ONLY);
+  gpio_set_pull_mode((gpio_num_t)ESP_CS_PIN,   GPIO_PULLUP_ONLY);
+
+  spi_slave_initialize(VSPI_HOST, &busCfg, &slvCfg, 1);
 
   while (1) {
     sendData();
@@ -91,31 +114,56 @@ void cleanDictionary() {
   }
 }
 
+void onSetupComplete(spi_slave_transaction_t*) {
+  xSemaphoreGiveFromISR(_readySemaphore, NULL);
+}
+
+int transfer(uint8_t out[], uint8_t in[], size_t len) {
+  spi_slave_transaction_t slvTrans;
+  spi_slave_transaction_t* slvRetTrans;
+
+  memset(&slvTrans, 0x00, sizeof(slvTrans));
+
+  slvTrans.length = len * 8;
+  slvTrans.trans_len = 0;
+  slvTrans.tx_buffer = out;
+  slvTrans.rx_buffer = in;
+
+  spi_slave_queue_trans(VSPI_HOST, &slvTrans, portMAX_DELAY);
+  xSemaphoreTake(_readySemaphore, portMAX_DELAY);
+  spi_slave_get_trans_result(VSPI_HOST, &slvRetTrans, portMAX_DELAY);
+  return (slvTrans.trans_len / 8);
+}
+
 void sendData() {
   if (digitalRead(ESP_CS_PIN) == LOW) {
-    uint8_t command = SPI.transfer(0);
-    if (command == 255) {
-      Serial.print("Command 255, sending ");
+    uint8_t commandBuffer[1] = { 0 };
+    int commandLength = transfer(NULL, commandBuffer, 1);
+    if (commandLength == 0) return;
+    Serial.print("Command ");
+    Serial.println(commandBuffer[0]);
+    if (commandBuffer[0] == 255) {
       int numKeys = packetMap.size();
       if (numKeys > 255) numKeys = 255;
-      Serial.println(numKeys);
-      SPI.transfer((uint8_t)numKeys);
-    } else if (command <= packetMap.size()) {
-      Serial.print("Command ");
-      Serial.println(command);
+      uint8_t responseBuffer[1] = { (uint8_t)numKeys };
+      transfer(responseBuffer, NULL, 1);
+    } else if (commandBuffer[0] <= packetMap.size()) {
       digitalWrite(ESP_BUSY_PIN, LOW);
       uint8_t index = 1;
       for (auto it = packetMap.begin(); it != packetMap.end(); it++) {
-        if (index == command) {
-          SPI.transfer((uint8_t)(it->second.w >> 8));
-          SPI.transfer((uint8_t)(it->second.w & 0xff));
-          SPI.transfer((uint8_t)(it->second.x >> 8));
-          SPI.transfer((uint8_t)(it->second.x & 0xff));
-          SPI.transfer((uint8_t)(it->second.y >> 8));
-          SPI.transfer((uint8_t)(it->second.y & 0xff));
-          SPI.transfer((uint8_t)(it->second.z >> 8));
-          SPI.transfer((uint8_t)(it->second.z & 0xff));
-          SPI.transfer(it->second.buttonPressed);
+        if (index == commandBuffer[0]) {
+          uint8_t responseBuffer[9] = { 
+            (uint8_t)(it->second.w >> 8), 
+            (uint8_t)(it->second.w & 0xff),
+            (uint8_t)(it->second.x >> 8), 
+            (uint8_t)(it->second.x & 0xff),
+            (uint8_t)(it->second.y >> 8), 
+            (uint8_t)(it->second.y & 0xff),
+            (uint8_t)(it->second.z >> 8), 
+            (uint8_t)(it->second.z & 0xff),
+            it->second.buttonPressed
+          };
+          transfer(responseBuffer, NULL, 9);
           break;
         }
         index++;
